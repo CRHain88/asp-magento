754f4607a7544e694296ab01250f5971f1cf9808
partht <parth.t@zapserver.com>
2016-12-09 16:08:49 +0530

    CSS and header changes

---
+++ src/magento2/app/code/Zaptech/Header/registration.php
@@ -0,0 +1,9 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Zaptech_Header',
+    __DIR__
+);
+
+?>
---
+++ src/magento2/app/design/frontend/CRHain/asp/Zaptech_Header/web/js/custom.js
@@ -0,0 +1,13 @@
+require([
+    "jquery"
+], function($){
+//<![CDATA[
+    $(document).ready(function() {
+        $('.ves-megamenu li').hover(function(){
+        	$('.nav-sections').addClass('hover-sctions');
+        },function(){
+        	$('.nav-sections').removeClass('hover-sctions');
+        });
+    });
+//]]>
+});
