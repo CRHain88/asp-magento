f4258c83186e9a9b3568b6366b7099ddf7ee49a3
Christian Hain <christian@crhain.com>
2016-12-13 14:40:15 -0500

    fix: adjust whitespace

--- src/magento2/app/design/frontend/CRHain/asp/Zaptech_Header/web/js/custom.js
+++ src/magento2/app/design/frontend/CRHain/asp/Zaptech_Header/web/js/custom.js
@@ -1,13 +1,11 @@
-require([
-    "jquery"
-], function($){
-//<![CDATA[
-    $(document).ready(function() {
-        $('.ves-megamenu li').hover(function(){
-        	$('.nav-sections').addClass('hover-sctions');
-        },function(){
-        	$('.nav-sections').removeClass('hover-sctions');
+require(['jquery'], function($) {
+    //<![CDATA[
+        $(document).ready(function () {
+            $('.ves-megamenu li').hover(function () {
+                $('.nav-sections').addClass('hover-sctions');
+            }, function () {
+                $('.nav-sections').removeClass('hover-sctions');
+            });
         });
-    });
-//]]>
+    //]]>
 });
