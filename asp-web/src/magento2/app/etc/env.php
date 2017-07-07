76c8fd5c9e305def0fc2d1a354d8328fe1de973d
Christian Hain <christian@crhain.com>
2017-07-07 11:31:49 -0400

    new: remove more git ignore

---
+++ asp-web/src/magento2/app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'kruger',
+  ),
+  'crypt' => 
+  array (
+    'key' => '8db0f812cf6d845020d06e5309c2df7d',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'db',
+        'dbname' => 'magento_asp_2',
+        'username' => 'asp_magento',
+        'password' => 'asp_magento',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 01 Feb 2017 23:28:10 +0000',
+  ),
+);
